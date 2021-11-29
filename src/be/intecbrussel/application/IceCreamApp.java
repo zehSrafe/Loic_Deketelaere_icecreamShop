f25ad8c5e42deac7b8dbeb81a018f2ba3adb3bdb
Loïc Deketelaere <zehsrafe@gmail.com>
2021-11-29 15:27:55 +0100

    two outa two

--- src/be/intecbrussel/application/IceCreamApp.java
+++ src/be/intecbrussel/application/IceCreamApp.java
@@ -3,9 +3,11 @@ package be.intecbrussel.application;
 import be.intecbrussel.eatables.Cone;
 import be.intecbrussel.eatables.IceRocket;
 import be.intecbrussel.eatables.Magnum;
+import be.intecbrussel.sellers.PriceList;
 
 public class IceCreamApp {
     public static void main(String[] args) {
+        PriceList priceList = new PriceList();
         IceRocket iceRocket = new IceRocket();
 
         Cone.Flavor[] flavors = {Cone.Flavor.BANANA, Cone.Flavor.LEMON, Cone.Flavor.CHOCOLATE};
@@ -16,5 +18,8 @@ public class IceCreamApp {
         iceRocket.eat();
         cone.eat();
         magnum.eat();
+        System.out.println(priceList.getRocketPrice());
+        System.out.println(priceList.getBallPrice() * flavors.length);
+        System.out.println(priceList.getMagnumPrice(Magnum.MagnumType.WHITECHOCOLATE));
     }
 }
--- src/be/intecbrussel/sellers/PriceList.java
+++ src/be/intecbrussel/sellers/PriceList.java
@@ -1,6 +1,50 @@
 package be.intecbrussel.sellers;
 
+import be.intecbrussel.eatables.Magnum;
+
 public class PriceList {
     private double ballPrice;
     private double rocketPrice;
+    private double magnumStandardPrice;
+
+    public PriceList(){
+        this(1.20, 1.35, 2.10);
+    }
+
+    public PriceList(double ballPrice, double rocketPrice, double magnumStandardPrice){
+        setBallPrice(ballPrice);
+        setRocketPrice(rocketPrice);
+        setMagnumStandardPrice(magnumStandardPrice);
+    }
+
+    public void setBallPrice(double ballPrice) {
+        this.ballPrice = ballPrice;
+    }
+
+    public void setRocketPrice(double rocketPrice) {
+        this.rocketPrice = rocketPrice;
+    }
+
+    public void setMagnumStandardPrice(double magnumStandardPrice) {
+        this.magnumStandardPrice = magnumStandardPrice;
+    }
+
+    public double getBallPrice() {
+        return ballPrice;
+    }
+
+    public double getRocketPrice() {
+        return rocketPrice;
+    }
+
+    public double getMagnumPrice(Magnum.MagnumType type) {
+        switch (type) {
+            case ROMANTICSTRAWBERRIES: return  magnumStandardPrice * 2;
+            case ALPINENUTS: return magnumStandardPrice * 1.5;
+            case MILKCHOCOLATE: return  magnumStandardPrice * 1.3;
+            case WHITECHOCOLATE: return  magnumStandardPrice * 1.05;
+            case BLACKCHOCOLATE: return  magnumStandardPrice;
+        }
+        return magnumStandardPrice;
+    }
 }
